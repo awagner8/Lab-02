4a90edc8f92a82fac957c9db87dfdbdc286182f6
Avi Wagner <avi.wagner@duke.edu>
2024-01-17 22:37:05 +0000

    Tried something

--- recurse_student.c
+++ recurse_student.c
@@ -2,7 +2,26 @@
 #include <stdlib.h>
 
 int my_recurse (int n) {
-  // YOUR CODE HERE... You should change the return value. This is here to avoid
-  // compilation errors.
+  
+  if (n == 0) {
+    return 0;
+  } else {
+    return my_recurse(n-1);
+  }
+}
+
+int main (int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <integer>\n", argv[0]);
+    return EXIT_SUCCESS;
+  }
+  else {
+    int val;
+    if ((val = atoi(argv[1])) == 0) {
+      fprintf(stderr, "ERROR: main(): atoi() error when reading args\n");
+      return EXIT_FAILURE;
+    }
+    printf("%d\n", my_recurse(val));
+  }
   return EXIT_SUCCESS;
 }
